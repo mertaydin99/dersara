6c0775dcbe903483ef0192b5399ae4aab6cfe8a5
mertaydin99 <mertaydin991@hotmail.com>
2022-03-27 01:11:32 +0300

    Additional fix for search_submit

--- project/public_html/public_html/search.php
+++ project/public_html/public_html/search.php
@@ -274,6 +274,7 @@ input:focus
 {
 	display: inline-block;
 	width: 10%;
+	height: 30px;
 	position: relative;
 	right: 11%;
 	cursor: pointer;
@@ -662,7 +663,7 @@ input:focus
 		right: 10%;
 		width: 30%;	
 		height: 6%;
-		margin-top: 2%;
+		margin-top: 1%;
 	}
 	.profile
 	{
@@ -922,7 +923,7 @@ input:focus
 			<div id="info_banner">Koronavirüsle ilgili çekinceleriniz varsa online özel ders veren öğretmenleri tercih edebilirsiniz.</div>
 			<div id="search">
 					<form id="search_form" method="GET" action="search.php"><input type="text" name="keyword" id="search_input" value="<?php echo $keyword?>" />
-						<button type="submit" id="search_submit">Aramaya Devam Edin</button>
+						<button type="submit" id="search_submit">Ara</button>
 					</form>
 			</div>
 			<div id="body" class="flex-container2">
